c4cecc817fed9acb19504394ce4e9bd8b5d2838d
León Abascal <leonabascal@opendeusto.es>
2021-04-06 01:27:16 +0200

    Soluciones a problemas de la entrada estándar de C
    Credits to: Andowna9@github.com
    https://github.com/Andowna9/

---
+++ console/adecuate_stdin.c
@@ -0,0 +1,32 @@
+void clear_stdin(); // Llamar cuando estemos seguros de que una limpieza de stdin es necesaria (si no puede pausar la ejecución esperando input)
+void clean_buffer(char* buffer); // Elimina el caracter de nueva línea de un buffer de caracteres (string)
+void scan_str(char* buffer, int buffer_size); // Forma segura de leer strings del teclado
+
+void clear_stdin() {
+    char c;
+    while((c = getchar()) != '\n'); // Se eliminan caracteres hasta alcanzar el salto de línea
+}
+
+void clean_buffer(char* buffer) {
+
+    int pos = strlen(buffer) - 1; // Buscamos el salto de línea en la penúltima posición (la última es '\0')
+    if (buffer[pos] == '\n') {
+        buffer[pos] = '\0'; // Si existe, se reemplaza por el caracter final
+    }
+
+    else {
+        clear_stdin(); // Ha habido un overflow, ya que no se ha incluido el salto de línea y es necesaria una limpieza
+    }
+}
+
+void scan_str(char* buffer, int buffer_size) { // Forma segura de leer strings del teclado
+
+    fgets(buffer, buffer_size, stdin);
+    clean_buffer(buffer);
+}
+
+void scan_str(char* buffer, int buffer_size) { // Forma segura de leer strings del teclado
+
+    fgets(buffer, buffer_size, stdin);
+    clean_buffer(buffer);
+}
